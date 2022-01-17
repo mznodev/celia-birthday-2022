4726bf5e73a90a3851363e71ffe9aad9d933d62b
mznodev <83369616+mznodev@users.noreply.github.com>
2022-01-17 18:13:56 +0100

    feat: game

--- src/index.js
+++ src/index.js
@@ -10,11 +10,10 @@ import WaitingScreen from "WaitingScreen";
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<WaitingScreen />} />
-      <Route path="PantallaPrincipal" element={<PantallaPrincipal />} />
-      <Route path="Game" element={<Game />} />
-      <Route path="WaitingScreen" element={<WaitingScreen />} />
-      WaitingScreen
+      <Route path="/" element={<PantallaPrincipal />} />
+      <Route path="pantallaPrincipal" element={<PantallaPrincipal />} />
+      <Route path="game" element={<Game />} />
+      <Route path="waitingScreen" element={<WaitingScreen />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
